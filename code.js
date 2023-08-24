17ad414e88cfb62b1f4da501f9bc34189f532526
SpiiderPlayz <98484252+Codero123@users.noreply.github.com>
2023-08-24 23:58:08 +0100

    Create code.js

---
+++ code.js
@@ -0,0 +1,7 @@
+//var browser = "chrome"; //Can only be chrome, edge, firefox or opera
+//var sinto = fetch("https://raw.githubuserinfo/miret/"+browser);
+//let curr = config.angetOverride.getRefreshValue(mainVal:NOW);
+//config.setRefreshValue(mainVal:NOW,newVal:sinto);
+location.reload();
+wait(1);
+console.log("Refresher by Spiider Playz");
